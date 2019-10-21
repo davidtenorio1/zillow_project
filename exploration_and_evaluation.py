d53cc7ebbab1c4c1b9b6e6f9f11dd82fd54bf114
davidtenorio1 <dtenorio2169@gmail.com>
2019-10-21 03:12:24 -0500

    visuals added

--- exploration_and_evaluation.py
+++ exploration_and_evaluation.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_style('whitegrid')
 import statsmodels.api as sm
@@ -38,6 +38,10 @@ X_train = train.drop(columns=["id", "taxvaluedollarcnt"])
 y_train = pd.DataFrame([train.taxvaluedollarcnt])
 y_train = y_train.transpose()
 
+X_test = test.drop(columns=["id", "taxvaluedollarcnt"])
+y_test = pd.DataFrame([test.taxvaluedollarcnt])
+y_test = y_test.transpose()
+
 X_train_scaled = split_scale.standard_scaler(X_train)
 #sns.heatmap(data.corr(), cmap='Blues', annot=True)
 
@@ -45,44 +49,44 @@ predictions=pd.DataFrame({'actual':y_train.taxvaluedollarcnt}).reset_index(drop=
 
 # model 1 using square feet only
 lm1=LinearRegression()
-lm1.fit(X_train[['calculatedfinishedsquarefeet']],y_train)
-lm1_predictions=lm1.predict(X_train[['calculatedfinishedsquarefeet']])
+lm1.fit(X_train_scaled[['calculatedfinishedsquarefeet']],y_train)
+lm1_predictions=lm1.predict(X_train_scaled[['calculatedfinishedsquarefeet']])
 predictions['lm1']=lm1_predictions
 
 # model 2 using square feet and bedroom count
 lm2=LinearRegression()
-lm2.fit(X_train[['calculatedfinishedsquarefeet', 'bedroomcnt']],y_train)
-lm2_predictions=lm2.predict(X_train[['calculatedfinishedsquarefeet', 'bedroomcnt']])
+lm2.fit(X_train_scaled[['calculatedfinishedsquarefeet', 'bedroomcnt']],y_train)
+lm2_predictions=lm2.predict(X_train_scaled[['calculatedfinishedsquarefeet', 'bedroomcnt']])
 predictions['lm2']=lm2_predictions
 
 # model 3 using all three variables
 lm3=LinearRegression()
-lm3.fit(X_train[['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt']],y_train)
-lm3_predictions=lm3.predict(X_train[['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt']])
+lm3.fit(X_train_scaled[['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt']],y_train)
+lm3_predictions=lm3.predict(X_train_scaled[['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt']])
 predictions['lm3']=lm3_predictions
 
 # model 4 using square feet and bathroom count
 lm4=LinearRegression()
-lm4.fit(X_train[['calculatedfinishedsquarefeet', 'bathroomcnt']],y_train)
-lm4_predictions=lm4.predict(X_train[['calculatedfinishedsquarefeet', 'bathroomcnt']])
+lm4.fit(X_train_scaled[['calculatedfinishedsquarefeet', 'bathroomcnt']],y_train)
+lm4_predictions=lm4.predict(X_train_scaled[['calculatedfinishedsquarefeet', 'bathroomcnt']])
 predictions['lm4']=lm4_predictions
 
 # model 5 using bedroom count and bathroom count
 lm5=LinearRegression()
-lm5.fit(X_train[['bedroomcnt', 'bathroomcnt']],y_train)
-lm5_predictions=lm5.predict(X_train[['bedroomcnt', 'bathroomcnt']])
+lm5.fit(X_train_scaled[['bedroomcnt', 'bathroomcnt']],y_train)
+lm5_predictions=lm5.predict(X_train_scaled[['bedroomcnt', 'bathroomcnt']])
 predictions['lm5']=lm5_predictions
 
 # model 6 using bedroom only
 lm6=LinearRegression()
-lm6.fit(X_train[['bedroomcnt']],y_train)
-lm6_predictions=lm6.predict(X_train[['bedroomcnt']])
+lm6.fit(X_train_scaled[['bedroomcnt']],y_train)
+lm6_predictions=lm6.predict(X_train_scaled[['bedroomcnt']])
 predictions['lm6']=lm6_predictions
 
 # model 7 using bathroom only
 lm7=LinearRegression()
-lm7.fit(X_train[['bathroomcnt']],y_train)
-lm7_predictions=lm7.predict(X_train[['bathroomcnt']])
+lm7.fit(X_train_scaled[['bathroomcnt']],y_train)
+lm7_predictions=lm7.predict(X_train_scaled[['bathroomcnt']])
 predictions['lm7']=lm7_predictions
 
 # baseline model
@@ -139,5 +143,23 @@ print(MSE_7,SSE_7,RMSE_7,r2_7)
 
 
 
+mvp_model=lm3.predict(X_test[['calculatedfinishedsquarefeet', 'bedroomcnt', 'bathroomcnt']])
+mvp_model=mvp_model.ravel().reshape(3205)
+y_test1=np.array(y_test).ravel().reshape(3205)
+baseline_predictions = predictions['baseline'].ravel().reshape(3205)
+best_model=pd.DataFrame({'model_predictions':mvp_model,'taxvaluedollarcnt':y_test1})
+
+best_model.head()
+
+
+sns.residplot(mvp_model, y_test)
+
+
+
+
+test_results=pd.DataFrame({'actual':y_test.taxvaluedollarcnt}).reset_index(drop=True)
+test_results['model_predictions'] = mvp_model
+test_results['baseline_predictions'] = y_test.mean()[0]
+sns.relplot(data=test_results)
 
 
